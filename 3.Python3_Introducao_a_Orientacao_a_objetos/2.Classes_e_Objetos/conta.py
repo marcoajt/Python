587710a9191693dd69f92b98e847bd5d58cd25f4
marcoajt <marcoantoniojt@gmail.com>
2022-04-03 22:41:44 -0300

    OO

---
+++ 3.Python3_Introducao_a_Orientacao_a_objetos/2.Classes_e_Objetos/conta.py
@@ -0,0 +1,7 @@
+
+
+from typing_extensions import Self
+
+
+class Conta:
+    pass
---
+++ 3.Python3_Introducao_a_Orientacao_a_objetos/2.Classes_e_Objetos/oo/conta.py
@@ -0,0 +1,13 @@
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.extrato = limite
+
+    def extrato(self):
+        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+
